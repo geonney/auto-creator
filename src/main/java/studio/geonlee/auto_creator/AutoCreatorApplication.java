0acc17b323826497c85d81da5311bf61a5ffeea6
unknown <geonlee@kakao.com>
2025-04-26 15:19:26 +0900

    [init] 테마 적용

---
+++ src/main/java/studio/geonlee/auto_creator/AutoCreatorApplication.java
@@ -0,0 +1,27 @@
+package studio.geonlee.auto_creator;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import studio.geonlee.auto_creator.frame.MainFrame;
+
+import javax.swing.*;
+
+//@SpringBootApplication
+public class AutoCreatorApplication {
+
+	public static void main(String[] args) {
+		
+		// 테마 적용
+		try {
+			UIManager.setLookAndFeel(new FlatDarculaLaf());
+		} catch (Exception e) {
+			System.err.println("FlatLaf 테마 적용 실패");
+		}
+
+//		SpringApplication.run(AutoCreatorApplication.class, args);
+		SwingUtilities.invokeLater(MainFrame::new); //MainFrame 실행
+	}
+
+}
