2bb160af8bc908d943edd4f7378dc55b1180292a
unknown <geonlee@kakao.com>
2025-04-30 07:04:52 +0900

    [feature] 최적/최저 사이즈 설정

--- src/main/java/studio/geonlee/auto_creator/ui/frame/MainFrame.java
+++ src/main/java/studio/geonlee/auto_creator/ui/frame/MainFrame.java
@@ -65,7 +65,7 @@ public class MainFrame extends JFrame {
 
 
         DefaultConfig config = DefaultConfigFileHandler.load();
-        setMinimumSize(new Dimension(1070, 800)); // 원하는 최소 사이즈 설정
+        setMinimumSize(new Dimension(1070, 800));
         if (config != null && config.getWindowWidth() > 0 && config.getWindowHeight() > 0) {
             setBounds(config.getWindowX(), config.getWindowY(), config.getWindowWidth(), config.getWindowHeight());
         } else {
